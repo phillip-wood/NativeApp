153f5bdfc871ba4c3f9760dd3cd7408226ff7b3a
phillip wood <phillip.wood369@gmail.com>
2021-01-14 09:41:51 +1300

    Revert "added static view profile page"
    This reverts commit 8dc3f8e59bb19e558ebd4a2347c26eb11e6bb3ad.

--- app/screens/3/ViewProfile.js
+++ app/screens/3/ViewProfile.js
@@ -1,55 +1,21 @@
 import React from "react"
-import { FlatList, StyleSheet, View } from "react-native"
+import { StyleSheet } from "react-native"
 
 import colors from "../../config/colors"
 import Screen from "../../components/Screen"
-import UserSnip from "../../components/UserSnip"
-import Card from "../../components/Card"
 
-const shoes = [
-  {
-    id: 1,
-    title: "Doc",
-    price: "Martin",
-    image: require("../../assets/shoe7.jpg"),
-  },
-  {
-    id: 2,
-    title: "Converse",
-    price: "All Stars",
-    image: require("../../assets/shoe5.jpg"),
-  },
-
-]
-
-export default function ViewProfile() {
+export default function Home() {
   return (
-    <Screen style={styles.screen}>
-      <View style={styles.container}>
-        <UserSnip title='Lara Simmons' subtitle='sneaker head' image={require('../../assets/user1.png')}/>
-      </View>
-      <View style={styles.container}>
-      <FlatList
-        data={shoes}
-        keyExtractor={(shoe) => shoe.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subTitle={item.price}
-            image={item.image}
-          />
-        )}
-      />
-      </View>
+    <Screen style={styles.container} >
+
     </Screen>
   )
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    backgroundColor: colors.primary,
-  },
+  
   container: {
-    marginVertical: 20,
+    backgroundColor: colors.black,
   },
+
 })
